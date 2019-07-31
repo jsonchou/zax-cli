5d9bd198036f033c43ff4bf79bf77f5f16f95d92
json chou <jsonchou329@gmail.com>
2019-07-31 14:24:12 +0800

    release: v1.0.3

--- scripts/release/index.js
+++ scripts/release/index.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const doneRainbow = require('done-rainbow')
 const execSync = require('child_process').execSync
-const { version } = require('../../../package.json')
+const { version } = require('../../package.json')
 
 let increaseVersion = () => {
     let prefix = version.slice(0, version.lastIndexOf('.'))
@@ -11,12 +11,6 @@ let increaseVersion = () => {
 
 let doRelease = async () => {
     let version = increaseVersion()
-    try {
-        execSync(`npm run build`, { stdio: 'inherit' })
-    } catch (err) {
-        console.log('build', err)
-        throw err
-    }
 
     try {
         execSync(`git add .`, { stdio: 'inherit' })
